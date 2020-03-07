6fb43f626e76f944c0e82751231ad8a083515081
gigarthan <gigarthan@parkwaylabs.com>
2020-03-07 20:14:24 +0530

    add challenge

--- functions/trigger.js
+++ functions/trigger.js
@@ -1,5 +1,9 @@
 exports.handler = function(event, context, callback) {
   // your server-side functionality
+  if (event.httpMethod === "GET") {
+    console.log("PUB SUB SUBSCRIBE");
+    return event.queryStringParameters["hub.challenge"];
+  }
   console.log("=============================");
   console.log(JSON.stringify(event));
   console.log("=============================");
