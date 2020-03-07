fce70f7ac87516621188e5cdf827fe25f889baa7
gigarthan <gigarthan@parkwaylabs.com>
2020-03-07 20:19:39 +0530

    chal

--- functions/trigger.js
+++ functions/trigger.js
@@ -1,12 +1,16 @@
 exports.handler = function(event, context, callback) {
   // your server-side functionality
-  if (event.httpMethod === "GET") {
-    console.log("PUB SUB SUBSCRIBE");
-    return event.queryStringParameters["hub.challenge"];
-  }
   console.log("=============================");
   console.log(JSON.stringify(event));
   console.log("=============================");
+  if (event.httpMethod === "GET") {
+    console.log("PUB SUB SUBSCRIBE");
+    console.log(event.queryStringParameters["hub.challenge"]);
+    return callback(null, {
+      statusCode: 200,
+      body: event.queryStringParameters["hub.challenge"]
+    });
+  }
   callback(null, {
     statusCode: 200,
     body: "Hello, World"
